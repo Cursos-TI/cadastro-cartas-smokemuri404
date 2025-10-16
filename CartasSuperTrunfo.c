4325cd560f1452d480d3a1afdb9d37d0fc37f50f
smokemuri404 <nicolasgomesf01@gmail.com>
2025-10-16 16:33:55 -0300

    Atualizar o CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,81 +3,81 @@
 
 int main() {
 
-    char Estado[20];
-    char Codigo[20];
-    char Nome[50];
-    int Populacao;
-    float Area;
+    char estado[20];
+    char codigo[20];
+    char nome[50];
+    int populacao;
+    float area;
     float PIB;
-    int Pontos_Turisticos;
+    int pontos_turisticos;
 
     printf("SEJA BEM-VINDO AO DESENVOLVIMENTO DO JOGO SUPER TRUNFO EM C!\n");
     printf("Nessa etapa você irá cadastrar dados de uma cidade com suas características que pontuarão no game!\n");
     printf("Bora lá!\n\n");
 
     printf("Digite o estado:\n");
-    scanf("%s", Estado);
+    scanf("%s", estado);
 
     printf("Digite o código que deseja dar à carta:\n");
-    scanf("%s", Codigo);
+    scanf("%s", codigo);
 
     printf("Digite o nome da cidade:\n");
-    scanf(" %[^\n]", Nome);  // permite nomes com espaço dica legal da net
+    scanf(" %[^\n]", nome);  // permite nomes com espaço dica legal da net
 
     printf("Digite sua população:\n");
-    scanf("%d", &Populacao);
+    scanf("%d", &populacao);
 
     printf("Digite sua área em km²:\n");
-    scanf("%f", &Area);
+    scanf("%f", &area);
 
     printf("Digite seu PIB:\n");
     scanf("%f", &PIB);
 
     printf("Digite quantos pontos turísticos existem:\n");
-    scanf("%d", &Pontos_Turisticos);
+    scanf("%d", &pontos_turisticos);
 
     printf("CARTA 1 CADASTRADA!\n");
-    printf("ESTADO: %s\n", Estado);
-    printf("CÓDIGO: %s\n", Codigo);
-    printf("CIDADE: %s\n", Nome);
-    printf("POPULAÇÃO: %d\n", Populacao);
-    printf("ÁREA: %.2f km²\n", Area);
+    printf("ESTADO: %s\n", estado);
+    printf("CÓDIGO: %s\n", codigo);
+    printf("CIDADE: %s\n", nome);
+    printf("POPULAÇÃO: %d\n", populacao);
+    printf("ÁREA: %.2f km²\n", area);
     printf("PIB: %.2f bilhões\n", PIB);
-    printf("PONTOS TURÍSTICOS: %d\n", Pontos_Turisticos);
+    printf("PONTOS TURÍSTICOS: %d\n", pontos_turisticos);
     
     printf("Perfeito!!! \n Agora vamos cadastrar mais uma carta!\n");
 
     printf("Bora lá!\n\n");
 
     printf("Digite o estado:\n");
-    scanf("%s", Estado);
+    scanf("%s", estado);
 
     printf("Digite o código que deseja dar à carta:\n");
-    scanf("%s",  Codigo);
+    scanf("%s",  codigo);
 
     printf("Digite o nome da cidade:\n");
-    scanf(" %[^\n]", Nome);  // permite nomes com espaço dica legal da net
+    scanf(" %[^\n]", nome);  // permite nomes com espaço dica legal da net
 
     printf("Digite sua população:\n");
-    scanf("%d", &Populacao);
+    scanf("%d", &populacao);
 
     printf("Digite sua área em km²:\n");
-    scanf("%f", &Area);
+    scanf("%f", &area);
 
     printf("Digite seu PIB:\n");
     scanf("%f", &PIB);
 
     printf("Digite quantos pontos turísticos existem:\n");
-    scanf("%d", &Pontos_Turisticos);
+    scanf("%d", &pontos_turisticos);
 
     printf("CARTA 2 CADASTRADA!\n");
-    printf("ESTADO: %s\n", Estado);
-    printf("CÓDIGO: %s\n", Codigo);
-    printf("CIDADE: %s\n", Nome);
-    printf("POPULAÇÃO: %d\n", Populacao);
-    printf("ÁREA: %.2f km²\n", Area);
+    printf("ESTADO: %s\n", estado);
+    printf("CÓDIGO: %s\n", codigo);
+    printf("CIDADE: %s\n", nome);
+    printf("POPULAÇÃO: %d\n", populacao);
+    printf("ÁREA: %.2f km²\n", area);
     printf("PIB: %.2f bilhões\n", PIB);
-    printf("PONTOS TURÍSTICOS: %d\n", Pontos_Turisticos);
+    printf("PONTOS TURÍSTICOS: %d\n", pontos_turisticos);
 
     return 0;
 }
